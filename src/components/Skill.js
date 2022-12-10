4a97112849187466d6f7e1031ba5074fecaaf52c
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-10 21:58:35 +0700

    add logo and import logo

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,3 +1,18 @@
+import html from "./img/logo/html.svg";
+import css from "./img/logo/css.svg";
+import javascript from "./img/logo/javascript.svg";
+import bootstrap from "./img/logo/bootstrap.svg";
+import tailwindcss from "./img/logo/tailwindcss.svg";
+import react from "./img/logo/react.svg";
+import github from "./img/logo/github.svg";
+import git from "./img/logo/git.svg";
+import vscode from "./img/logo/vscode.svg";
+
 export default function Skill() {
-  return <h1 className="text-5xl mt-10 text-teal-500 font-bold text-center">Skill</h1>;
+  return (
+    <div className="container">
+      <h1 className="text-5xl mt-10 text-primary font-bold text-center">My Skill</h1>
+      <img src={css} width="50" />
+    </div>
+  );
 }
