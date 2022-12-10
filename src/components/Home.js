8670765c947d09846188d1c08fdc4c7f6c752b8c
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-10 16:50:22 +0700

    fix home

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,15 +16,16 @@ export default function Home() {
         </span>
       </div>
       <div className="relative w-full lg:w-1/2">
-        <div className="absolute left-[320px] w-72 border-4 border-slate-900 ">
-          <h1 className="px-5 py-2 text-center   text-xl font-bold">Welcome to my profile</h1>
+        <div className="absolute left-[340px] w-72 border-[3px] border-slate-900">
+          <div className="border-2 border-slate-100 ">
+            <h1 className="px-5 py-2 text-center  text-xl font-bold bg-gradient-to-r from-backgroundColor to-indigo-800 text-white">Welcome to my profile</h1>
+          </div>
         </div>
         <h1 className="mt-16 text-right text-4xl font-bold">Hi, I'm Agung Saputra</h1>
-        <h1 className="mt-4 text-right">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quidem laboriosam illum aliquid pariatur ipsa quo, laborum, eligendi, ullam ea totam voluptates adipisci? Aut nihil quis optio, error est, inventore ut,
-          aspernatur id quae quidem veritatis incidunt ipsa eius architecto quas molestias impedit cumque praesentium tempora accusantium reiciendis nulla
-        </h1>
-        <h1 className="mt-10 text-right">Let's Connect</h1>
+        <h1 className="mt-4 absolute text-lg left-[150px] w-[480px] text-right">I am a semester 1 student from Indonesia. I am interested in programming, especially fullstack developer. My daily life is studying and playing games.</h1>
+        <a href="https://wa.me/+62895337305533" target="_blank" rel="noreferrer" className="absolute text-lg mt-36 left-[495px] text-right hover:text-hoverColor1 cursor-pointer">
+          Let's Connect ➜
+        </a>
       </div>
     </div>
   );
