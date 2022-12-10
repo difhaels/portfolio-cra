8ac417a8063000f79fd5f99a0e5903061c5dc3bd
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-10 22:16:54 +0700

    add prototype skill and fix bug

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -12,7 +12,30 @@ export default function Skill() {
   return (
     <div className="container">
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">My Skill</h1>
-      <img src={css} width="50" />
+
+      <div className="flex items-center justify-center gap-14">
+        <div className="flex items-center">
+          <img src={html} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">60%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={css} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">60%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={html} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">60%</h1>
+        </div>
+      </div>
     </div>
   );
 }
