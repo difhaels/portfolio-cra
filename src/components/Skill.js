62eddea33c8962c4a2af58f4a494968e218d67f7
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-10 22:28:25 +0700

    add prototype skill and fix bug

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -13,27 +13,73 @@ export default function Skill() {
     <div className="container">
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">My Skill</h1>
 
-      <div className="flex items-center justify-center gap-14">
+      <div className="pt-14 flex items-center justify-center gap-14">
         <div className="flex items-center">
           <img src={html} width="60" className="mr-2" />
           <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+            <div className="bg-primary w-[95%] h-[9px] rounded-full"></div>
           </div>
-          <h1 className="p-3">60%</h1>
+          <h1 className="p-3">95%</h1>
         </div>
         <div className="flex items-center">
           <img src={css} width="60" className="mr-2" />
           <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+            <div className="bg-primary w-[90%] h-[9px] rounded-full"></div>
           </div>
-          <h1 className="p-3">60%</h1>
+          <h1 className="p-3">90%</h1>
         </div>
         <div className="flex items-center">
-          <img src={html} width="60" className="mr-2" />
+          <img src={javascript} width="60" className="mr-2" />
           <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-            <div className="bg-primary w-[60%] h-[9px] rounded-full"></div>
+            <div className="bg-primary w-[30%] h-[9px] rounded-full"></div>
           </div>
-          <h1 className="p-3">60%</h1>
+          <h1 className="p-3">30%</h1>
+        </div>
+      </div>
+      <div className="pt-10 flex items-center justify-center gap-14">
+        <div className="flex items-center">
+          <img src={bootstrap} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[40%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">40%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={tailwindcss} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[90%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">90%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={react} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[10%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">10%</h1>
+        </div>
+      </div>
+      <div className="pt-10 flex items-center justify-center gap-14">
+        <div className="flex items-center">
+          <img src={vscode} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[80%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">80%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={git} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[20%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">20%</h1>
+        </div>
+        <div className="flex items-center">
+          <img src={github} width="60" className="mr-2" />
+          <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
+            <div className="bg-primary w-[45%] h-[9px] rounded-full"></div>
+          </div>
+          <h1 className="p-3">45%</h1>
         </div>
       </div>
     </div>
