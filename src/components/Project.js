75c6961978135c83d1ec2a8dc7938829c91f2f15
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-11 17:34:30 +0700

    add project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,13 @@
 export default function Project() {
-  return <h1 className="text-5xl mt-10 text-red-500 font-bold text-center">Projects</h1>;
+  return (
+    <div>
+      <h1 className="text-5xl mt-10 text-primary font-bold text-center">Projects</h1>
+      <div className="w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28">
+        <h1 className="text-xl">I've done a lot of small projects</h1>
+        <h1 className="text-xl">but I don't think it's worth putting it here</h1>
+        <h1 className="text-xl pt-8">So what if working together makes something interesting?</h1>
+        <h1 className="text-xl">interesting, Right?</h1>
+      </div>
+    </div>
+  );
 }
