aae03129df425b0616549a489dd28f1c3b181602
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-12 21:47:37 +0700

    add emote 404

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ export default function Project() {
   return (
     <div>
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">Projects</h1>
-      <div className="w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28 group hover:bg-purple-500 ">
+      <div className="lg:w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28 group hover:bg-purple-500 mx-5">
         <h1 className="text-xl group-hover:text-slate-50">I've done a lot of small projects</h1>
         <h1 className="text-xl group-hover:text-slate-50">but I don't think it's worth putting it here</h1>
         <h1 className="text-xl pt-8 group-hover:text-slate-50">So what if working together makes something interesting?</h1>
