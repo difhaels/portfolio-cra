162fd874c94bb9be4bde2efea801bec2df68eed2
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-12 20:48:10 +0700

    added hover in skill and project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,11 +2,11 @@ export default function Project() {
   return (
     <div>
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">Projects</h1>
-      <div className="w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28">
-        <h1 className="text-xl">I've done a lot of small projects</h1>
-        <h1 className="text-xl">but I don't think it's worth putting it here</h1>
-        <h1 className="text-xl pt-8">So what if working together makes something interesting?</h1>
-        <h1 className="text-xl">interesting, Right?</h1>
+      <div className="w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28 group hover:bg-purple-500 ">
+        <h1 className="text-xl group-hover:text-slate-50">I've done a lot of small projects</h1>
+        <h1 className="text-xl group-hover:text-slate-50">but I don't think it's worth putting it here</h1>
+        <h1 className="text-xl pt-8 group-hover:text-slate-50">So what if working together makes something interesting?</h1>
+        <h1 className="text-xl group-hover:text-slate-50">interesting, Right?</h1>
       </div>
     </div>
   );
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -14,108 +14,108 @@ export default function Skill() {
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">My Skill</h1>
 
       <div className="pt-14 flex items-center justify-center gap-8">
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500 ">
           <div className="flex items-center ">
             <img src={html} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[95%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[95%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">95%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">95%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">i'm pretty good at html, </h1>
-          <h1 className="flex justify-center text-lg ">i also almost master all elements in html</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">i'm pretty good at html, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">i also almost master all elements in html</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={css} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[90%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[90%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">90%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">90%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">i'm pretty good at css, </h1>
-          <h1 className="flex justify-center text-lg ">i know what i'm doing</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">i'm pretty good at css, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">i know what i'm doing</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={javascript} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[30%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[30%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">30%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">30%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">Still learning, </h1>
-          <h1 className="flex justify-center text-lg ">still don't know what i'm doing</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">Still learning, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">still don't know what i'm doing</h1>
         </div>
       </div>
       <div className="pt-10 flex items-center justify-center gap-8">
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500 ">
           <div className="flex items-center">
             <img src={bootstrap} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[40%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[40%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">40%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">40%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">seldom use it, </h1>
-          <h1 className="flex justify-center text-lg ">maybe i forgot</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">seldom use it, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">maybe i forgot</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={tailwindcss} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[90%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[90%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">90%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">90%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">already accustomed, </h1>
-          <h1 className="flex justify-center text-lg ">my main css frameworks</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">already accustomed, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">my main css frameworks</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={react} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[10%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[10%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">10%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">10%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">my main js library, </h1>
-          <h1 className="flex justify-center text-lg ">but still learning</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">my main js library, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">but still learning</h1>
         </div>
       </div>
       <div className="pt-10 flex items-center justify-center gap-8">
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={vscode} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[80%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[80%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">80%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">80%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">my main code editor, </h1>
-          <h1 className="flex justify-center text-lg ">best code editor</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">my main code editor, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">best code editor</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={git} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[20%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[20%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">20%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">20%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">Still learning, </h1>
-          <h1 className="flex justify-center text-lg ">no caption</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">Still learning, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">no caption</h1>
         </div>
-        <div className="border-4 bg-purple-200 rounded-xl p-5 ">
+        <div className="border-4 bg-purple-200 rounded-xl p-5 group hover:bg-purple-500">
           <div className="flex items-center">
             <img src={github} width="60" className="mr-2" />
             <div className="w-[200px] h-4 border-4 border-slate-600 flex items-center rounded-full">
-              <div className="bg-primary w-[45%] h-[9px] rounded-full"></div>
+              <div className="bg-primary w-[45%] h-[9px] rounded-full group-hover:bg-red-500"></div>
             </div>
-            <h1 className="p-3">45%</h1>
+            <h1 className="p-3 group-hover:text-slate-50">45%</h1>
           </div>
-          <h1 className="flex justify-center text-lg pt-4">use it often, </h1>
-          <h1 className="flex justify-center text-lg ">but not for teamwork</h1>
+          <h1 className="flex justify-center text-lg pt-4 group-hover:text-slate-50">use it often, </h1>
+          <h1 className="flex justify-center text-lg group-hover:text-slate-50">but not for teamwork</h1>
         </div>
       </div>
     </div>
