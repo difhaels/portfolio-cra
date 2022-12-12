55572cb20c9528864c170b4fa549838d7d9e79a1
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-12 21:51:53 +0700

    change project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,11 @@
+import emote404 from "./img/emote404.jpg";
+
 export default function Project() {
   return (
     <div>
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">Projects</h1>
-      <div className="lg:w-[600px] border-4 bg-purple-200 rounded-xl p-5 flex flex-wrap justify-center mx-auto my-28 group hover:bg-purple-500 mx-5">
-        <h1 className="text-xl group-hover:text-slate-50">I've done a lot of small projects</h1>
-        <h1 className="text-xl group-hover:text-slate-50">but I don't think it's worth putting it here</h1>
-        <h1 className="text-xl pt-8 group-hover:text-slate-50">So what if working together makes something interesting?</h1>
-        <h1 className="text-xl group-hover:text-slate-50">interesting, Right?</h1>
+      <div className="flex justify-center my-28">
+        <img src={emote404} alt="404" width="200" />
       </div>
     </div>
   );
