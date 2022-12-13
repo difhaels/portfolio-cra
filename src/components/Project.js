401afecbf8d39c90f77ea39506ac72b15398bc04
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-13 20:47:13 +0700

    remove tag in index html

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ export default function Project() {
   return (
     <div>
       <h1 className="text-5xl mt-10 text-primary font-bold text-center">Projects</h1>
-      <div className="flex justify-center my-28">
+      <div className="flex justify-center my-72">
         <img src={emote404} alt="404" width="200" />
       </div>
     </div>
