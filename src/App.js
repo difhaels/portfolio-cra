82e065fd5ee6ed7fae70d8f9eae1e8e2a0a879ef
Agung Saputra <agungsaputradifh@gmail.com>
2022-12-18 21:38:58 +0700

    fix when scroll

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="mt-5 ">
+    <div className=" ">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
